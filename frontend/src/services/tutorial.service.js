50509f791daa48a3db338023dd96dfd90fc401ba
Avinash <uic.18mca1021@gmail.com>
2026-05-03 11:04:47 +0530

    Full migration to MongoDB, Functional Components, and Professional Structure

---
+++ frontend/src/services/tutorial.service.js
@@ -0,0 +1,33 @@
+import axios from "./axios-common";
+
+class TutorialDataService {
+  getAll() {
+    return axios.get("/tutorials");
+  }
+
+  get(id) {
+    return axios.get(`/tutorials/${id}`);
+  }
+
+  create(data) {
+    return axios.post("/tutorials", data);
+  }
+
+  update(id, data) {
+    return axios.put(`/tutorials/${id}`, data);
+  }
+
+  delete(id) {
+    return axios.delete(`/tutorials/${id}`);
+  }
+
+  deleteAll() {
+    return axios.delete(`/tutorials`);
+  }
+
+  findByTitle(title) {
+    return axios.get(`/tutorials?title=${title}`);
+  }
+}
+
+export default new TutorialDataService();
